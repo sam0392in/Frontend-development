18d97de1b7156f1431babcd891d34f911435cdde
Samarth Kanungo <sam@Samarths-MBP.Dlink>
2020-10-06 12:25:27 +0530

    completed till JS strings , need to continue from JS String methods

---
+++ js-learning-modules/js_functions.js
@@ -0,0 +1,24 @@
+
+function myFunction(p1, p2) {
+  return p1 * p2;   // The function returns the product of p1 and p2
+}
+
+function name(parameter1, parameter2, parameter3) {
+  // code to be executed
+}
+
+
+// Function call
+var x = myFunction(4, 3);
+
+
+//-------example---------------
+
+function toCelsius(fahrenheit) {
+  toCelsius = (5/9) * (fahrenheit-32);
+  return Celsius
+}
+
+var fahrenheit = 80;
+Celsius_value = toCelsius(fahrenheit)
+document.getElementById("demo").innerHTML = Celsius_value;
